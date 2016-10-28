f9f7f7133dcfd7dcc44f6962585ac16d31712c49
Fabio Fehr <Fabio Fehr>
2016-10-27 17:08:50 -0700

    created skeleton for bubbles

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var PORT = 5000;
 var express = require('express');
 // Print logs to the console and compress pages we send
 var logger = require('logger');
-var compress = require('compress');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
@@ -23,9 +22,10 @@ var app = express();
 // whenever they are requested at '/'
 // e.g., http://localhost:3000/index.html
 // maps to /static/index.html on this machine
-app.use(express.static(__dirname + 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', index.view);
 app.get('/index', index.view)
@@ -34,10 +34,8 @@ app.get('/settings', settings.view);
 app.get('/subpage1', subpage1.view);
 app.get('/subpage2', subpage2.view);
 
-
-
 // Start the server
-var port = process.env.PORT || PORT; // 80 for web, 3000 for development
+var port = process.env.PORT || PORT; // 80 for web, 5000 for development
 http.createServer(app).listen(port, function() {
 	console.log("Node.js server running on port %s", port);
 });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@
 /*
  * GET home page.
  */
+data = require('../data.json')
 
 exports.view = function(req, res){
-  	res.render('index');
+  	res.render('index', data);
 };
