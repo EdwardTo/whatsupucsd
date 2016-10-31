62db7f7d6e150ea2a28a36bf1b195c96c01c3a17
Isaac Fehr <ifehr@ucsd.edu>
2016-10-31 14:55:26 -0700

    bubbles now resize by nYesses

--- public/js/bubbles.js
+++ public/js/bubbles.js
@@ -1,12 +1,19 @@
-var startHeight = 300;
-var sizeDivider = 50;
+var topicStartHeight = 300;
+var subtopicStartHeight = 100;
+var topicSizeDivider = 50;
+var subtopicSizeDivider = 100;
 
 $(document).ready(function(){
 	
-
-	$(".topic").each(function( index ) {
-		var bubbleCirc = (startHeight + (parseInt($(this).find(".title .badge").text(), 10) / sizeDivider ));
-		$(this).css({ 'height': bubbleCirc + "px", 'width': bubbleCirc + "px" });
+	//Vary bubble circumference by nYesses
+	$(".topic").each(function( i ) {
+		var topicCirc = (topicStartHeight + (parseInt($(this).find(".title .badge").text(), 10) / topicSizeDivider ));
+		$(this).css({ 'height': topicCirc + "px", 'width': topicCirc + "px" });
+		//Resize subtopic bubbles
+		$(this).find(".subtopic").each(function( j ){
+			var subtopicCirc = (subtopicStartHeight + (parseInt($(this).find(".badge").text(), 10) / subtopicSizeDivider ));
+			$(this).css({ 'height': subtopicCirc + "px", 'width': subtopicCirc + "px" });
+		});
 	});
 
 	$("addYes").click(function(){
