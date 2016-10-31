5c187d940baae9714e39adbeba3ece0977acd469
Fabio Fehr <Fabio Fehr>
2016-10-30 17:33:58 -0700

    topic traversal is now expandable and badges were added

--- routes/newtopic.js
+++ routes/newtopic.js
@@ -5,5 +5,8 @@
 data = require('../data.json')
 
 exports.view = function(req, res){
+	console.log(data);
+	var newTopic = {title: req.query.problem, id:"./" + req.query.problem};//, description: req.query.description};
+	data.topics.push(newTopic);
   	res.render('newtopic', data);
 };
