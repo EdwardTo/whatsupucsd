79eb22d85f0de0301d058be8ec315652dedf21cc
Isaac Fehr <ifehr@ucsd.edu>
2016-10-31 09:08:26 -0700

    'yo'

--- routes/newtopic.js
+++ routes/newtopic.js
@@ -5,11 +5,8 @@
 data = require('../data.json')
 
 exports.view = function(req, res){
-<<<<<<< HEAD
 	console.log(data);
 	var newTopic = {'title': req.query.problem, 'description': req.query.description};
-=======
-
   	res.render('newtopic');
 };
 
@@ -17,7 +14,6 @@ exports.submit = function(req, res){
 	console.log('KJSDFKLDSJFKLSDJ req = ' + req.body.problem);
 		//console.log(data);
 	var newTopic = {title: req.body.problem, id:"./" + req.body.problem};//, description: req.query.description};
->>>>>>> refs/remotes/origin/master
 	data.topics.push(newTopic);
 	res.redirect('index');
 };
