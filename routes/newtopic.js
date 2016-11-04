4068111e794363bdb4477bd8f0b16abccda17212
Isaac Fehr <ifehr@ucsd.edu>
2016-11-03 22:14:39 -0700

    fixed more bugs

--- routes/newtopic.js
+++ routes/newtopic.js
@@ -13,32 +13,38 @@ exports.view = function(req, res){
 exports.submit = function(req, res){
 	var topicPath = req.body.URL;
 	var topicParams = topicPath.split('/');
+	topicParams.shift();
+	console.log("topic params: " + topicParams.toString());
 	topicParams.pop();
-	var newTopic = {title: req.body.problem, modifiers: {important: 0, agree: 0, disagree: 0, unimportant: 0, love: 0}};
+	var newTopic = {title: req.body.problem, modifiers: {important: 0, agree: 0, disagree: 0, unimportant: 0, love: 0}, topics: []};
 	pushTopicData(topicParams, newTopic);
 	topicParams = topicParams.join('/');
-	res.redirect(topicParams);
+	res.redirect('/' + topicParams);
 };
 
 function pushTopicData(topicParams, newTopic){
 	currTopicLevel = data;
-
-	for(var i = 0; i < topicParams.length; i++){
-		for(var j = 0; j < currTopicLevel.topics.length; j++){
-			var obj = currTopicLevel.topics[j];
-			if(obj.title == topicParams[i]){
-				if(i == (topicParams.length - 1)){
-					obj.topics.push(newTopic);
-					return;
+	if(topicParams.length == 0){
+		data.topics.push(newTopic);
+	}
+	else{
+		for(var i = 0; i < topicParams.length; i++){
+			for(var j = 0; j < currTopicLevel.topics.length; j++){
+				var obj = currTopicLevel.topics[j];
+				if(obj.title == topicParams[i]){
+					if(i == (topicParams.length - 1)){
+						obj.topics.push(newTopic);
+						return;
+					}
+					else{
+						currTopicLevel = obj;
+						break;
+					}	
 				}
-				else{
-					currTopicLevel = obj;
-					break;
-				}	
 			}
 		}
 	}
-	return {};
+return {};
 }
 
 function getTopicData(topicParams){
